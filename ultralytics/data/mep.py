94c3f4e0e39d82cee160e89149278dc5429e3f36
IceSandwich <cxndream@gmail.com>
2026-02-24 17:54:11 +0800

    update mep

--- ultralytics/data/mep.py
+++ ultralytics/data/mep.py
@@ -102,57 +102,63 @@ def main(args):
 	folder_path: str = args.dataset
 	# 遍历文件夹
 	index = 1
-	for filename in os.listdir(folder_path):
-		name, ext = os.path.splitext(filename)
+	modes: typing.List[str] = os.listdir(folder_path)
+	for mode in modes:
+		fn_path = os.path.join(folder_path, mode)
+		target_img_path = os.path.join(args.output, "images", mode)
+		os.makedirs(target_img_path, exist_ok=True)
+		target_txt_path = os.path.join(args.output, "labels", mode)
+		os.makedirs(target_txt_path, exist_ok=True)
+		for filename in os.listdir(fn_path):
+			name, ext = os.path.splitext(filename)
 
-		# 检查是否为图片
-		if ext.lower() not in image_extensions:
-			continue
+			# 检查是否为图片
+			if ext.lower() not in image_extensions:
+				continue
 
-		image_path = os.path.join(folder_path, filename)
-		img = Image.open(image_path)
+			image_path = os.path.join(fn_path, filename)
+			img = Image.open(image_path)
 
-		webpIO = io.BytesIO()
-		if args.use_webp:
-			if image_path.endswith('.webp'):
-				with open(image_path, 'rb') as f:
-					webpIO = io.BytesIO(f.read())
+			webpIO = io.BytesIO()
+			if args.use_webp:
+				if image_path.endswith('.webp'):
+					with open(image_path, 'rb') as f:
+						webpIO = io.BytesIO(f.read())
+				else:
+					img.save(webpIO, format="WebP", quality=95)
+			img.close()
+
+			mep_img_path = f"dat{index}.mep"
+			outputfn = os.path.join(target_img_path, mep_img_path)
+			if args.use_webp:
+				data = encode(webpIO.getvalue())
 			else:
-				img.save(webpIO, format="WebP", quality=95)
-		img.close()
+				data = encode(image_path)
+			with open(outputfn, 'wb') as f:
+				f.write(data)
 
-		mep_img_path = f"dat{index}.mep"
-		target_txt_path = os.path.join(args.output, f"dat{index}.txt")
-		outputfn = os.path.join(args.output, mep_img_path)
-		if args.use_webp:
-			data = encode(webpIO.getvalue())
-		else:
-			data = encode(image_path)
-		with open(outputfn, 'wb') as f:
-			f.write(data)
-
-		txt_filename = f"{name}.txt"
-		txt_path = os.path.join(folder_path, txt_filename)
-		
-		# 检查并读取对应的文本文件
-		if os.path.exists(txt_path):
-			with open(txt_path, 'r', encoding='utf-8') as f:
-				prompt = f.read().strip()
-		else:
-			print("没有找到对应的txt文件：", name)
-			raise Exception("Not found correspondent txt file.")
-		
-		with open(target_txt_path, 'w', encoding='utf-8') as f:
-			f.write(prompt)
-		
-		print(f"-- Convert {image_path} => {mep_img_path}")
-		index = index + 1
+			txt_path = os.path.join(fn_path, f"{name}.txt")
+			
+			# 检查并读取对应的文本文件
+			if os.path.exists(txt_path):
+				with open(txt_path, 'r', encoding='utf-8') as f:
+					prompt = f.read().strip()
+			else:
+				print("没有找到对应的txt文件：", name)
+				raise Exception(f"Not found correspondent txt file {txt_path}.")
+			
+			output_txt_path = os.path.join(target_txt_path, f"dat{index}.txt")
+			with open(output_txt_path, 'w', encoding='utf-8') as f:
+				f.write(prompt)
+			
+			print(f"-- Convert {image_path} => {mep_img_path}")
+			index = index + 1
 
 	print(f"- Converted {index - 1} images.")
 
 	#check
 	if index - 1 > 0:
-		sample_fn = os.path.join(args.output, "dat1.mep")
+		sample_fn = os.path.join(args.output, "images", modes[0], "dat1.mep")
 		try:
 			mat = CVRead(sample_fn)
 			if not args.no_preview:
